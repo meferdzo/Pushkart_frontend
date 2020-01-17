887cb5ebd25d990c93f2fceedbd1c3d7cd3b9e7c
FerjancicR <rok.ferjancic20@gmail.com>
2020-01-17 14:33:11 +0100

    Initial Commit.

---
+++ core/_config/demo1/menu.config.ts
@@ -0,0 +1,27 @@
+export class MenuConfig {
+	public defaults: any = {
+		aside: {
+			self: {},
+			items: [
+				{
+					title: 'Sestavi sporočilo',
+					root: true,
+					icon: 'flaticon2-architecture-and-city',
+					page: 'dashboard',
+					translate: 'MENU.DASHBOARD',
+					bullet: 'dot',
+				},
+				{
+					title: 'Layout Builder',
+					root: true,
+					icon: 'flaticon2-expand',
+					page: 'builder'
+				},
+			]
+		},
+	};
+
+	public get configs(): any {
+		return this.defaults;
+	}
+}
---
+++ views/pages/dashboard/dashboard.module.ts
@@ -0,0 +1,34 @@
+// Angular
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+// Core Module
+import { CoreModule } from '../../../core/core.module';
+import { PartialsModule } from '../../partials/partials.module';
+import { DashboardComponent } from './dashboard.component';
+import { HttpClientModule} from '@angular/common/http';
+import { AngularEditorModule } from '@kolkov/angular-editor';
+
+@NgModule({
+	imports: [
+		CommonModule,
+		PartialsModule,
+		CoreModule,
+		HttpClientModule,
+		AngularEditorModule,
+		RouterModule.forChild([
+			{
+				path: '',
+				component: DashboardComponent
+			},
+		]),
+	],
+	providers: [],
+	bootstrap: [DashboardComponent],
+	declarations: [
+		DashboardComponent,
+	]
+})
+export class DashboardModule {
+}
